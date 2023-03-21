b455192ea71ce601198af379bf34562a62048e60
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-21 16:40:55 +0100

    finished registeration

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ var express = require("express");
 var router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// sqlite3 mydatabase.db "DELETE FROM users;"
 
 const db = new sqlite3.Database("mydatabase.db");
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT UNIQUE, password TEXT)"
+  );
+});
+
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
@@ -20,20 +29,44 @@ router.post("/register", async (req, res) => {
     return res.status(400).send("Password cannot be empty");
   }
   try {
-    db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
-      if (err) {
-        console.log(err.message);
-        return res.status(500).send("Server error");
-      }
-      if (row) {
-        return res
-          .status(400)
-          .send("User with this email already exist in our data");
-      }
+    const alreadyExists = await new Promise((resolve, reject) => {
+      db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
+        if (err) {
+          console.log(err.message);
+          reject(err);
+          return res.status(500).send("Server error");
+        } else if (row) {
+          console.log(row);
+          resolve(row);
+          return res
+            .status(400)
+            .send("User with this email already exist in our data");
+        } else {
+          resolve(false);
+        }
+      });
     });
+    if (alreadyExists) {
+      return res.status(400).send("User already exist in the database");
+    }
     const encryptedPassword = await bcrypt.hash(password, 10);
-    console.log(encryptedPassword);
-  } catch (err) {}
+    db.run(
+      "INSERT INTO users (name, email, password) VALUES (? , ? , ?)",
+      [name, email, encryptedPassword],
+      (err) => {
+        if (err) {
+          console.log(err.message);
+          return res.status(500).send("Server error");
+        }
+        const token = jwt.sign({ email }, "secret_key");
+        return res
+          .status(201)
+          .json({ message: "User created successfully", token });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
