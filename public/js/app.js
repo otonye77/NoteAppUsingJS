d55d4a32344c61c14eea019dee627f5c63c80c50
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-21 12:26:49 +0100

    connected frontend to backend

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,24 @@ const nameInput = document.querySelector('input[type="text"]');
 const emailInput = document.querySelector('input[type="email"]');
 const passwordInput = document.querySelector('input[type="password"]');
 
-formEl.addEventListener("submit", (e) => {
+formEl.addEventListener("submit", async(e) => {
   e.preventDefault();
   const formData = {
     name: nameInput.value,
     email: emailInput.value,
     password: passwordInput.value
   }
-  console.log(formData);
+  try {
+    const response = await fetch("http://localhost:7000/users/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    throw new Error("Data cannot be created")
+  }
 });
