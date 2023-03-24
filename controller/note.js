165f4050b8b28ebfb66ec4f5fcabbaf8182820a2
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-24 01:16:34 +0100

    implemented delete logic

--- controller/note.js
+++ controller/note.js
@@ -2,11 +2,21 @@
 const Note = require("../model/notesModel");
 const { v4: uuidv4 } = require("uuid");
 
+const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.findAll();
+    return res.status(200).json({
+      data: notes,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const createNotes = async (req, res) => {
   const { title, description, date, status } = req.body;
   try {
     let userId = req.user;
-    console.log(userId);
     if (!userId) {
       return res.status(404).send("You cannot create note");
     }
@@ -18,16 +28,71 @@ const createNotes = async (req, res) => {
       status,
       userId,
     });
-    console.log(note);
     return res.status(201).json({
       message: `${note.title} has been created successfully`,
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.status(500).json({
       err: "Server Error",
     });
   }
 };
 
-module.exports = { createNotes };
+const deleteNotes = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const userId = req.user;
+    const note = await Note.findOne({ where: { id: noteId } });
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    if (note.userId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this code" });
+    }
+    await note.destroy();
+    return res.status(200).json({ message: "Note deleted successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      err: "Server Error",
+    });
+  }
+};
+
+const updateNotes = async (req, res) => {
+  const { title, description, date, status } = req.body;
+  try {
+    const noteId = req.params.id;
+    const userId = req.user;
+    const note = await Note.findOne({ where: { id: noteId } });
+    if (!note) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    if (note.userId !== userId) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to update this note" });
+    }
+
+    await note.update({
+      title,
+      description,
+      date,
+      status,
+    });
+
+    return res.status(200).json({
+      message: `${note.title} has been updated successfully`,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      err: "Server Error",
+    });
+  }
+};
+
+module.exports = { getNotes, createNotes, deleteNotes, updateNotes };
--- routes/note.js
+++ routes/note.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createNotes } = require("../controller/note");
+const { createNotes, deleteNotes, getNotes, updateNotes } = require("../controller/note");
 const { authenticatedUser } = require("../middleware/index");
 
+router.get("/notes", getNotes)
 router.post("/create", authenticatedUser,  createNotes);
+router.delete("/delete/:id", authenticatedUser, deleteNotes);
+router.put("/update/:id", authenticatedUser, updateNotes)
 
 module.exports = router;
